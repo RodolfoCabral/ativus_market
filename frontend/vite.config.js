f2455560269c938de81e2682f922947a187777ff
Rodolfo Cabral <rodolfocabral@outlook.com.br>
2025-10-06 18:18:00 -0300

    Ajustando os diretorios das blueprint

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+export default {
+  base: './',
+  build: {
+    outDir: 'dist',
   },
-})
+};
